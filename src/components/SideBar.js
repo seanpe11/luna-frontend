031ae5cbded3e4dee8ecf1d70a3b0a513f6f805a
jolocansana <jcansana@gmail.com>
2022-01-02 01:29:14 +0800

    dynamic sidebar done

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,26 +1,55 @@
+import notdone from '../images/progress-toggle-notdone.png'
+import inprogress from '../images/progress-toggle-inprogress.png'
+import complete from '../images/progress-toggle-complete.png'
 import logo from '../images/moon-full-moon-icon.png'
+import line from '../images/line.png'
+
+function progressChecker (progress) {
+    switch (progress) {
+        case 'notdone':
+            return (<img src={notdone} alt='logo' width='50px'/>)
+        case 'inprogress':
+            return (<img src={inprogress} alt='logo' width='50px'/>)
+        case 'done':
+            return (<img src={complete} alt='logo' width='50px'/>)
+        default:
+            return
+    }
+}
+
 
 function SideBar() {
-    let items=[{
-        title: 'yeboi',
-        next: false
-    }];
+    let items=[
+        {
+            title: 'Symptoms',
+            desc: 'Input the symptoms you are currently experiencing',
+            progress: 'done',
+        },
+        {
+            title: 'Preference',
+            desc: 'List what types of doctors you prefer',
+            progress: 'inprogress'
+        },
+        {
+            title: 'Confirm Inputs',
+            desc: 'Confirm your inputs to generate the most appropriate list of doctors to consult with',
+            progress: 'notdone'
+        },
+    ];
 
     let itemList=items.map((item,index)=>{
         return (
             <>
-                <div className='w-100 mb-4 row'>
+                <div className='w-100 mb-1 row'>
                     <div className='col-3'>
-                        <img src={logo} alt='logo' width='50px'/>
+                        {progressChecker(item.progress)}
+                        {
+                            index !== items.length-1 && <img src={line} alt='logo' width='50px'/>
+                        }  
                     </div>
                     <div className='col-9'>
                         <span style={{fontWeight: 'bolder'}}>{item.title}</span> <br/>
-                        <span style={{fontWeight: 'bolder'}}>This is a long description for the process that is currently being done.</span>
-                        {
-                            item.next ?
-                                <span>True</span> :
-                                <span>False</span>
-                        }             
+                        <span>{item.desc}</span>           
                     </div> 
                 </div>
             </>
