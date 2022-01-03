b0628fd20bea78271484ef4426ad8437241280c6
seanpe11 <sean.m.s.pe@gmail.com>
2022-01-03 15:43:01 +0800

    added styling for select buttons

--- src/views/FindMe2.js
+++ src/views/FindMe2.js
@@ -7,8 +7,8 @@ function FindMe() {
             <SideBar />
             <div class="findMe-wrapper col-8">
                 <select class="findMe-select" multiple>
-                    <option value="500">Less than PHP 500.00</option>
-                    <option value="1000">PHP 500.00 - PHP 1,000.00</option>
+                    <option class="findMe-option-thicc" value="500">Less than PHP 500.00</option>
+                    <option value="1000">PHP 500.00 to PHP 1,000.00</option>
                     <option value="2000">PHP 1,000 to PHP 2,000.00</option>
                     <option value="2001">More than PHP 2,000.00</option>
                     <option value="-1">No Preference</option>
