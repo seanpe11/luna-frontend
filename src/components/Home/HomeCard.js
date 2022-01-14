3be7a5ae20f40e539a3d767d38733be67086ad2b
jolocansana <jcansana@gmail.com>
2022-01-14 11:49:41 +0800

    home styling fixes

--- src/components/Home/HomeCard.js
+++ src/components/Home/HomeCard.js
@@ -4,19 +4,19 @@ import dp from "../../images/profile_pic.png";
 function HomeCard() {
   return (
     <div class="home-card text-white mb-3">
-      <div class="home-row g-0">
-        <div class="col-md-2 d-inline-flex justify-content-center">
+      <div class="home-row g-0 d-flex h-100 align-items-center">
+        <div class="ms-3">
           <img
-            className="dp m-5 rounded-pill"
+            className="rounded-pill"
             src={dp}
             alt="profile pic"
             style={{ width: "200px" }}
           />
         </div>
-        <div class="col-md-10 position-relative">
-          <div class="card-body position-absolute " style={{ top: "20%" }}>
+        <div class="ms-5">
+          <div class="card-body" style={{ top: "20%" }}>
             <h5 class="wc card-title ">Welcome,</h5>
-            <p class="name card-text ">Jill Doe</p>
+            <p class="name card-text ">Jane Doe</p>
           </div>
         </div>
       </div>
